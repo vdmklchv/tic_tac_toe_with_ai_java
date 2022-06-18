29fccf19239f57eea844290b07454533c0ffaade
Vadim Colcev <vadimkolchev@gmail.com>
2022-06-18 12:55:06 +0300

    Added enum import to PlayerCreator class

--- PlayerCreator.java
+++ PlayerCreator.java
@@ -1,17 +1,19 @@
 package tictactoe;
 
+import tictactoe.Enums.*;
+
 class PlayerCreator {
      Player make(String option, String token) {
         if ("user".equals(option)) {
             return new HumanPlayer(token);
         }
         if ("easy".equals(option)) {
-            return new ComputerPlayer(token, Enums.GAME_LEVEL.EASY);
+            return new ComputerPlayer(token, GAME_LEVEL.EASY);
         }
         if ("medium".equals(option)) {
-            return new ComputerPlayer(token, Enums.GAME_LEVEL.MEDIUM);
+            return new ComputerPlayer(token, GAME_LEVEL.MEDIUM);
         } else {
-            return new ComputerPlayer(token, Enums.GAME_LEVEL.HARD);
+            return new ComputerPlayer(token, GAME_LEVEL.HARD);
         }
     }
 }
